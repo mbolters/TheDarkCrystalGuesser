6111952d27f88186150becfc0552959ca7a42cd7
mbolters <mariematisans@gmail.com>
2019-08-17 17:40:21 -0500

    button coding to myscore box and random number generators

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,104 @@
 $(document).ready(function() {
 
-    var random = Math.floor(Math.random()*102+19);
+//sets variable crystalValues to random number globally
+let crystalValue1 = Math.floor(Math.random()*12+1);
+let crystalValue2 = Math.floor(Math.random()*12+1);
+let crystalValue3 = Math.floor(Math.random()*12+1);
+let crystalValue4 = Math.floor(Math.random()*12+1);
 
+let myScore = (0);
 
+//add a global variable for myscore then in the onclick to the image to += plus the number generated and print to score html box
 
-$("#numberWins").text(wins);
-$("#numberLosses").text(losses);
-var crystalValues = {};
-crystalValues[1] = Math.floor(Math.random()*12+1);
-crystalValues[2] = Math.floor(Math.random()*12+1);
-crystalValues[3] = Math.floor(Math.random()*12+1);
-crystalValues[4] = Math.floor(Math.random()*12+1);
-
-$("#crystalValues").text(random);
-var userTotal= 0;
-var wins = 0;
-var losses = 0;
-
-
-function getCrystalHandler(crystalKey) {
-   return function() {
-    userTotal = userTotal + crystalValues[crystalKey];
-    console.log("New userTotal " + userTotal);
-    $("#score").text(userTotal);
-
-    if (userTotal === random) {
-        winner()
-    }
-
-    else if (userTotal > random) {
-        loser()
-    } 
-}
-}
-
-$("#image1").on("click", getCrystalHandler(1));
-$("#image2").on("click", getCrystalHandler(2));
-$("#image3").on("click", getCrystalHandler(3));
-$("#image4").on("click", getCrystalHandler(4));
-
-
-function reset() {
-random = Math.floor(Math.random()*102+19);
-console.log(random);
-$("#crystalValues").text(random);
-var num1 = Math.floor(Math.random()*12+1);
-var num2 = Math.floor(Math.random()*12+1);
-var num3 = Math.floor(Math.random()*12+1);
-var num4 = Math.floor(Math.random()*12+1);
-userTotal = 0;
-$("#score").text(userTotal);
-}
-
-function winner() {
-alert("You Won!!");
-wins++;
-$("#numberWins").text(wins);
-reset();
-}
-
-function loser() {
-    
-losses++;
-$("#numberLosses").text(losses);
-reset();
-}
-
-
-$("#image1").on("click", getCrystalHandler(1));
-$("#image2").on("click", getCrystalHandler(2));
-$("#image3").on("click", getCrystalHandler(3));
-$("#image4").on("click", getCrystalHandler(4));
+$("#image1").click(function (){
+  myScore += crystalValue1; 
+  $("#score").text(myScore);
 });
+
+$("#image2").click(function(){
+    myScore += crystalValue2;
+    $("#score").text(myScore);
+});
+
+$("#image3").click(function(){
+    myScore += crystalValue3;
+    $("#score").text(myScore);
+});
+
+$("#image4").click(function(){
+    myScore += crystalValue4;
+    $("#score").text(myScore);
+});
+
+
+// this generates a random number into the matchNumber box every time I refresh the page
+
+function getNumber() {
+ let random = Math.floor(Math.random()*121+19);
+return random;
+
+}
+
+let matchNumber = getNumber();
+$('#randomNumber').html(matchNumber);
+
+});
+
+
+// let crystalValue1 = crystalNumber();
+// $('#score').html(crystalValue1);
+// $("#image1").on("click", (crystalValue1));
+
+// $("#numberWins").text(wins);
+// $("#numberLosses").text(losses);
+
+
+// $("#crystalValues").text(randomNumber);
+// var userTotal= 0;
+// var wins = 0;
+// var losses = 0;
+
+
+// function getCrystalHandler(crystalKey) {
+//    return function() {
+//     userTotal = userTotal + crystalValues[crystalKey];
+//     console.log("New userTotal " + userTotal);
+//     $("#score").text(userTotal);
+
+//     if (userTotal === randomNumber) {
+//         winner()
+//     }
+
+//     else if (userTotal > randomNumber) {
+//         loser()
+//     } 
+// }
+// }
+
+
+// function reset() {
+// randomNumber = Math.floor(Math.random()*100+1);
+// console.log(random);
+// $("#crystalValues").text(randomNumber);
+// var num1 = Math.floor(Math.random()*10+1);
+// var num2 = Math.floor(Math.random()*10+1);
+// var num3 = Math.floor(Math.random()*10+1);
+// var num4 = Math.floor(Math.random()*10+1);
+// userTotal = 0;
+// $("#score").text(userTotal);
+// }
+
+// function winner() {
+// alert("You Won!!");
+// wins++;
+// $("#numberWins").text(wins);
+// reset();
+// }
+
+// function loser() {
+// alert("You Lost!!");
+// losses++;
+// $("#numberLosses").text(losses);
+// reset();
+// }
+
