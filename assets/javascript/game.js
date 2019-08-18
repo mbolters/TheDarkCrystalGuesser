c667c06f0e1d3953d4662864d799a2d3315b7878
mbolters <mariematisans@gmail.com>
2019-08-17 23:20:08 -0500

    finishing touches to functionality and style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,33 @@ let crystalValue1 = Math.floor(Math.random()*12+1);
 let crystalValue2 = Math.floor(Math.random()*12+1);
 let crystalValue3 = Math.floor(Math.random()*12+1);
 let crystalValue4 = Math.floor(Math.random()*12+1);
+let wins = 0;
+let losses = 0;
+let myScore = 0;
 
-let myScore = (0);
 
 //add a global variable for myscore then in the onclick to the image to += plus the number generated and print to score html box
 
+function crystals(crystalValue){
+    myScore += crystalValue; 
+    $("#score").text(myScore);
+    reset();
+}
+
 $("#image1").click(function (){
-  myScore += crystalValue1; 
-  $("#score").text(myScore);
+    crystals(crystalValue1);
 });
 
 $("#image2").click(function(){
-    myScore += crystalValue2;
-    $("#score").text(myScore);
+    crystals(crystalValue2);
 });
 
 $("#image3").click(function(){
-    myScore += crystalValue3;
-    $("#score").text(myScore);
+    crystals(crystalValue3);
 });
 
 $("#image4").click(function(){
-    myScore += crystalValue4;
-    $("#score").text(myScore);
+    crystals(crystalValue4);
 });
 
 
@@ -42,63 +46,26 @@ return random;
 let matchNumber = getNumber();
 $('#randomNumber').html(matchNumber);
 
+//reset and win/lose code
+function reset(){
+    if (matchNumber === myScore) {
+        wins++;
+        $("#wins").html("Wins: " + wins);
+        scoreReset();
+        
+    } else if (matchNumber < myScore) {
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        scoreReset();
+
+    }
+}
+function scoreReset(){
+    myScore = 0;
+    $("#score").text(myScore);
+    matchNumber = getNumber();
+    $('#randomNumber').html(matchNumber);
+}
 });
 
-
-// let crystalValue1 = crystalNumber();
-// $('#score').html(crystalValue1);
-// $("#image1").on("click", (crystalValue1));
-
-// $("#numberWins").text(wins);
-// $("#numberLosses").text(losses);
-
-
-// $("#crystalValues").text(randomNumber);
-// var userTotal= 0;
-// var wins = 0;
-// var losses = 0;
-
-
-// function getCrystalHandler(crystalKey) {
-//    return function() {
-//     userTotal = userTotal + crystalValues[crystalKey];
-//     console.log("New userTotal " + userTotal);
-//     $("#score").text(userTotal);
-
-//     if (userTotal === randomNumber) {
-//         winner()
-//     }
-
-//     else if (userTotal > randomNumber) {
-//         loser()
-//     } 
-// }
-// }
-
-
-// function reset() {
-// randomNumber = Math.floor(Math.random()*100+1);
-// console.log(random);
-// $("#crystalValues").text(randomNumber);
-// var num1 = Math.floor(Math.random()*10+1);
-// var num2 = Math.floor(Math.random()*10+1);
-// var num3 = Math.floor(Math.random()*10+1);
-// var num4 = Math.floor(Math.random()*10+1);
-// userTotal = 0;
-// $("#score").text(userTotal);
-// }
-
-// function winner() {
-// alert("You Won!!");
-// wins++;
-// $("#numberWins").text(wins);
-// reset();
-// }
-
-// function loser() {
-// alert("You Lost!!");
-// losses++;
-// $("#numberLosses").text(losses);
-// reset();
-// }
 
